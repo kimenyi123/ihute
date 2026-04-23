388e16afede6241fc0026375a3ba4813ba3a3f4a
Vincent Company <vincentcompany634@gmail.com>
2026-04-23 19:20:23 +0200

    resolved seller dashboard

--- app/admin/sellers/page.tsx
+++ app/admin/sellers/page.tsx
@@ -352,7 +352,7 @@ export default function SellersPage() {
                     Seller
                   </th>
                   <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Contact
+                    Contacts
                   </th>
                   <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                     Location
